9583230405b209fc4a89e13c85864a0a0d84baa2
Sakina Mbatha <sakina@natix.io>
2023-08-22 13:14:38 +0200

    refactor: displaying an image

--- lib/gradient_wrapper.dart
+++ lib/gradient_wrapper.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/styled_text.dart';
+// import 'package:my_app/styled_text.dart';
 
 const beginAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
@@ -17,30 +17,13 @@ class GradientWrapper extends StatelessWidget {
         gradient: LinearGradient(
             begin: beginAlignment, end: endAlignment, colors: [color1, color2]),
       ),
-      child: const Center(
-        child: StyledText('Hey there, Jane Doe'),
+      child: Center(
+        // child: StyledText('Hey there, Jane Doe'),
+        child: Image.asset(
+          'assets/images/dice-1.png',
+          width: 170,
+        ),
       ),
     );
   }
 }
-
-// class GradientWrapper extends StatelessWidget {
-//   const GradientWrapper({super.key, required this.colors});
-
-//   final List<Color> colors;
-
-//   @override
-//   Widget build(context) {
-//     return Container(
-//       decoration: BoxDecoration(
-//         gradient: LinearGradient(
-//             begin: beginAlignment, end: endAlignment, colors: colors),
-//       ),
-//       child: const Center(
-//         child: StyledText('Hey there, Jane Doe'),
-//       ),
-//     );
-//   }
-// }
-
-// [Colors.amber, Color.fromARGB(255, 197, 63, 221)]
